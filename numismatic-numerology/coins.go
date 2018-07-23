c527dec9ab0d1ee3cb784d3219369fbb84aecb1b
Phil Killewald <pkillewald@mathematica-mpr.com>
2018-07-23 10:56:59 -0400

    Allowing for manual coin lists.

--- numismatic-numerology/coins.go
+++ numismatic-numerology/coins.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"strconv"
+	"strings"
 )
 
 func min(i, j int) int {
@@ -56,8 +57,7 @@ func findBest(nCoins, maxValue int) []bestCoins {
 	best := bestCoins{avgCoins: float64(maxValue), coins: make([]int, nCoins)}
 	out := make([]bestCoins, 0)
 	for c := range coins {
-		m := coinChangeMatrix(c, maxValue)
-		a := mean(m[nCoins][1:])
+		a := avgValue(c, maxValue)
 		if a <= best.avgCoins {
 			newBest := bestCoins{avgCoins: a, coins: c}
 			if a < best.avgCoins {
@@ -72,31 +72,93 @@ func findBest(nCoins, maxValue int) []bestCoins {
 	return out
 }
 
+func avgValue(coins []int, maxValue int) float64 {
+	m := coinChangeMatrix(coins, maxValue)
+	return mean(m[len(coins)][1:])
+}
+
+var nCoins int
+var maxValue int
+
+type coinValues []int
+
+var manualCoinValues coinValues
+
+// String implements flag.Value interface
+func (c *coinValues) String() string {
+	return fmt.Sprint(*c)
+}
+
+// Set implements flag.Value interface
+func (c *coinValues) Set(value string) error {
+	for _, v := range strings.Split(value, ",") {
+		coin, err := strconv.Atoi(v)
+		if err != nil {
+			return err
+		}
+		*c = append(*c, coin)
+	}
+	return nil
+}
+
+func init() {
+	flag.IntVar(&maxValue, "m", 100, "Value for which to make change")
+	flag.IntVar(&nCoins, "n", 0, "Number of coins to optimize")
+	flag.Var(&manualCoinValues, "v", "Comma-separated values of coins for manual calculation")
+}
+
 func main() {
 
-	nCoins, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		panic("unable to parse number of coins")
-	}
+	flag.Parse()
+	doManual := false
 
-	maxValue, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		panic("unable to parse maximum value")
-	}
-
-	if nCoins < 1 {
+	if nCoins < 1 && manualCoinValues == nil {
 		panic("number of coins cannot be less than 1")
 	}
 
-	smallestSum := 0
-	for i := 1; i <= nCoins; i++ {
-		smallestSum += i
-	}
-	if maxValue < smallestSum {
-		panic("maximum value too small for given number of coins")
+	if manualCoinValues != nil && nCoins != 0 {
+		panic("specify only one of flag -n or -v")
 	}
 
-	best := findBest(nCoins, maxValue)
-	fmt.Printf("Best overall:\n%v", best)
+	if nCoins == 0 {
+		doManual = true
+	}
+
+	if doManual {
+		smallestV := -1
+		duplicateV := make(map[int]bool)
+		for _, v := range manualCoinValues {
+			if v <= 0 {
+				panic("coin values must be greater than 0")
+			}
+			if _, isSet := duplicateV[v]; isSet {
+				panic("coin values must be unique")
+			}
+			duplicateV[v] = true
+			if smallestV < 0 || v < smallestV {
+				smallestV = v
+			}
+		}
+		if smallestV != 1 {
+			fmt.Println("WARNING: Smallest coin value must be 1.  Value of 1 will be added to coin list automatically.")
+			manualCoinValues = append(manualCoinValues, 1)
+		}
+	} else {
+		smallestSum := 0
+		for i := 1; i <= nCoins; i++ {
+			smallestSum += i
+		}
+		if maxValue < smallestSum {
+			panic("maximum value too small for given number of coins")
+		}
+	}
+
+	if doManual {
+		a := avgValue(manualCoinValues, maxValue)
+		fmt.Printf("Average number of coins required for %v to make change for %d:\n%v\n", manualCoinValues, maxValue, a)
+	} else {
+		best := findBest(nCoins, maxValue)
+		fmt.Printf("Best overall %d coins for making change for %d:\n%v\n", nCoins, maxValue, best)
+	}
 
 }
