5054d4b0cd5b9adb6635780554c40c5c9b0d7494
Phil Killewald <reallyasi9@users.noreply.github.com>
2022-01-19 17:20:26 -0500

    Add wordle solution attempt

--- wordle/wordle.jl
+++ wordle/wordle.jl
@@ -1,6 +1,7 @@
 using ArgParse
-using Combinatorics
 using ProgressMeter
+using Combinatorics
+using Random
 
 function parse_arguments(args=ARGS)
     s = ArgParseSettings()
@@ -18,91 +19,136 @@ function parse_arguments(args=ARGS)
     return parsed_args
 end
 
+struct Word
+    letters::NTuple{5, UInt8}
+end
+
+function Word(word::String)
+    @inbounds Word((word[1], word[2], word[3], word[4], word[5]) .- '`')
+end
+
+function Base.show(io::IO, x::Word)
+    show(io, String(collect(x.letters .+ '`')))
+end
+
+function Base.print(io::IO, x::Word)
+    print(io, String(collect(x.letters .+ '`')))
+end
+
+import Base.getindex
+function Base.getindex(w::Word, i)
+    return getindex(w.letters, i)
+end
+
+function Base.getindex(w::Word, cs::NTuple{5, Bool})
+    return getindex(w.letters, collect(cs))
+end
+
+function Base.isdisjoint(x::Word, y::Word)
+    return isdisjoint(x.letters, y.letters)
+end
 struct Wordle
-    lettermap::Dict{Char,Vector{String}}
-    invmap::Dict{Char,Vector{String}}
-    posmap::Dict{Tuple{Char,Int},Vector{String}}
-    words::Vector{String}
+    posmap::NTuple{5,NTuple{26,BitSet}}
+    lettermap::NTuple{26,BitSet}
+    missingmap::NTuple{26,BitSet}
+    words::Vector{Word}
+end
+
+function nwords(w::Wordle)
+    return length(w.words)
+end
+
+function getword(w::Wordle, i)
+    return w.words[i]
+end
+
+function wordswithletter(w::Wordle, c::UInt8)
+    return @inbounds w.lettermap[c]
+end
+
+function wordsmissingletter(w::Wordle, c::UInt8)
+    return @inbounds w.missingmap[c]
+end
+
+function wordsmatching(w::Wordle, c::Pair{Int,UInt8})
+    return @inbounds w.posmap[c[1]][c[2]]
 end
 
 const ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
 function Wordle(words)
-    lettermap = Dict{Char,Vector{String}}(
-        c => String[] for c in ALPHABET
-    )
-    invmap = Dict{Char,Vector{String}}(
-        c => String[] for c in ALPHABET
-    )
-    posmap = Dict{Tuple{Char,Int},Vector{String}}()
-    for c in ALPHABET
-        for i in 1:5
-            posmap[(c,i)] = String[]
-        end
-    end
-    for word in words
-        for letter in ALPHABET
-            if letter in word
-                push!(lettermap[letter], word)
-                for pos in findall(letter, word)
-                    push!(posmap[(letter, pos)], word)
+    lettermap = ntuple(x->BitSet(), 26)
+    missingmap = ntuple(x->BitSet(), 26)
+    posmap = ntuple(y->ntuple(x -> BitSet(), 26), 5)
+    for i in eachindex(words)
+        for c in ALPHABET
+            char = c - 'a' + 1
+            if c in words[i]
+                push!(lettermap[char], i)
+                for pos in findall(c, words[i])
+                    push!(posmap[pos][char], i)
                 end
             else
-                push!(invmap[letter], word)
+                push!(missingmap[char], i)
             end
         end
     end
-    return Wordle(lettermap, invmap, posmap, words)
+
+    return Wordle(posmap, lettermap, missingmap, Word.(words))
 end
 
-function guess(target::String, guesses)
-    combined = reduce(*, guesses)
-    letters = intersect(target, combined)
-    misses = setdiff(combined, target)
-
-    matches = Int[]
-    for guess in guesses
-        append!(matches, findall(collect(target) .== collect(guess)))
-    end
-    matchtupples = [(target[i],i) for i in unique(matches)]
-    
-    return letters, misses, matchtupples
+function Base.show(io::IO, w::Wordle)
+    show(io, w.words)
 end
 
-function guess(target::String, guess::String)
-    return guess(target, (guess,))
+function guess(target::Word, g::Word)
+    matches = g.letters .== target.letters
+    contains = in.(g.letters, (target.letters,))
+    return [i=>g[i] for i in findall(matches)], [g[i] for i in findall(contains)], [g[i] for i in findall(.!contains)]
 end
 
-function ambiguities(w::Wordle, target::String, guesses)
-    letters, misses, matchtupples = guess(target, guesses)
-    wlm = isempty(letters) ? String[] : mapreduce(x->get(w.lettermap, x, ""), intersect, letters)
-    winv = isempty(misses) ? String[] :  mapreduce(x->get(w.invmap, x, ""), intersect, misses)
-    wmatch = isempty(matchtupples) ? String[] : mapreduce(x->get(w.posmap, x, ""), intersect, matchtupples)
-    return intersect(wlm, winv, wmatch)
+function emptyintersect(s, itrs...)
+    isempty(s) && return intersect(itrs...)
+    return intersect(s, itrs...)
+end
+
+function possibilities(w::Wordle, matches::Vector{Pair{Int,UInt8}}, contains::Vector{UInt8}, misses::Vector{UInt8})
+    ma = mapreduce((c)->wordsmatching(w, c), emptyintersect, matches; init=BitSet())
+    co = mapreduce((c)->wordswithletter(w, c), emptyintersect, contains; init=ma)
+    return mapreduce((c)->wordsmissingletter(w, c), emptyintersect, misses; init=co)
+end
+
+function ambiguities(w::Wordle, target::Word, gs::Vector{Word})
+    possible = mapreduce(x->possibilities(w, guess(target, x)...), intersect, gs)
+    1 / length(possible)
 end
 
 function main(args=ARGS)
     a = parse_arguments(args)
 
     solutions = readlines(a["solutions"])
-    guessables = readlines(a["guessables"])
+    guessables = Word.(readlines(a["guessables"]))
 
     wordle = Wordle(solutions)
     combs = collect(combinations(guessables, 2))
-    # ncombs = length(combs)
-    ncombs = 100
-    ambicount = Vector{Tuple{Int,String,String}}(undef, ncombs)
-    p = Progress(ncombs)
-    Threads.@threads for i in 1:ncombs
-        guesses = combs[i]
-        n = mapreduce(x->length(ambiguities(wordle, x, guesses)), +, solutions; init=0)
-        ambicount[i] = (n, guesses...)
-        next!(p)
+    filter!(x->isdisjoint(x...), combs)
+    shuffle!(combs)
+    ncombs = length(combs)
+    # ncombs = 100
+    best_combo = first(combs)
+    best_prob = 0.
+    p = Progress(ncombs; showspeed=true)
+    solutions = Word.(solutions)
+    Threads.@threads for combo in combs
+        prob = mapreduce(x -> ambiguities(wordle, x, combo), +, solutions)
+        if prob > best_prob
+            best_prob = prob
+            best_combo = combo
+        end
+        next!(p; showvalues=[(:best_combo,join(best_combo, " + ")), (:prob,best_prob/length(solutions))])
     end
-
-    sort!(ambicount; lt = (x,y) -> x[1] < y[1])
-    println(first(ambicount, 100))
-    return ambicount
+    return best_combo, best_prob
 end
 
-ambicount = main();
+# combs, ambicount = main();
+main()
