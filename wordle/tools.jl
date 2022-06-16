3c1fd0c3bbcc2503d64ba15aad93448526740999
Phil Killewald <reallyasi9@users.noreply.github.com>
2022-06-16 12:46:31 -0400

    Add more running solutions

--- wordle/tools.jl
+++ wordle/tools.jl
@@ -1,3 +1,7 @@
+function parsewordle(word)
+    [i => word[i] for i in 1:length(word) if word[i] ∈ 'a':'z']
+end
+
 is(letters) = function(word)
     all([word[pos] == letter for (pos, letter) in letters])
 end
@@ -8,6 +12,12 @@ end
 
 hasnot(letters) = function(word)
     all([letter ∉ word for letter in letters])
+end
+
+function wordle(iswords::Vector{String}, haswords::Vector{String}, hasnotletters, words)
+    isletters = mapreduce(parsewordle, vcat, iswords)
+    hasletters = mapreduce(parsewordle, vcat, haswords)
+    return wordle(isletters, hasletters, hasnotletters, words)
 end
 
 wordle(isletters, hasletters, hasnotletters, words) = filter(hasnot(hasnotletters), filter(has(hasletters), filter(is(isletters), words)))
